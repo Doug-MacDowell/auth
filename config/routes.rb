1ad9617ead3bd13e8befb70a09a1ebf2320aa393
Doug MacDowell <dougmac9@uw.edu>
2014-02-14 22:22:46 -0800

    add sessions resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Auth::Application.routes.draw do
+
   resources :users
+  resources :sessions
+
+  root :to => 'welcome#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
