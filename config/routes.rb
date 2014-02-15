fbbde0d3d10d4da536442f8015510dce00097772
Doug MacDowell <dougmac9@uw.edu>
2014-02-14 22:35:33 -0800

    added custom routes to make nice URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Auth::Application.routes.draw do
 
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   resources :users
   resources :sessions
 
   root :to => 'welcome#index'
+#  resources :welcome
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
